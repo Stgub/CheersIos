00ccd7cc9784f8fd31b533694d8ad26b18024ecd
Charles Fayal <Charles@macs-iMac.local>
2017-07-11 17:27:02 -0400

    Changed email, and time

--- Cheers/ConfigUtil.swift
+++ Cheers/ConfigUtil.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class ConfigUtil{
     public static let MONTH_IN_SEC:TimeInterval = 60*60*24*30
-    
+    public static let CONTACT_EMAIL = "Contact@TheToastClub.com"
     static let SERVER_BASE = "http://ec2-34-202-232-113.compute-1.amazonaws.com:5000"   // AWS server 34.202.232.113*
     //static let SERVER_BASE = "http://0.0.0.0:5000" //local test
     
