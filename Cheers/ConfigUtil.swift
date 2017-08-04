376caf11367a554746e934aa8c90a854107c4d05
Charles <charles.fayal@uconn.edu>
2017-08-04 16:50:47 -0400

    server update

--- Cheers/ConfigUtil.swift
+++ Cheers/ConfigUtil.swift
@@ -18,8 +18,8 @@ class ConfigUtil{
     public static let TERMS_AND_CONDITIONS_URL = "http://thetoastclub.com/terms-and-conditions/"
     public static let FAQ_URL = "http://www.thetoastclub.com/faq/"
     
-    //static let SERVER_BASE = "http://ec2-34-202-232-113.compute-1.amazonaws.com:5000"   // AWS server 34.202.232.113*
-    static let SERVER_BASE = "http://0.0.0.0:5000" //local test
+    static let SERVER_BASE = "http://ec2-34-202-232-113.compute-1.amazonaws.com:5000"   // AWS server 34.202.232.113*
+    //static let SERVER_BASE = "http://0.0.0.0:5000" //local test
     
     static let STRIPE_DEV_KEY =  "pk_test_vbIvPl0JdKlVqMlrILx3A8Sf" //test
     static let STRIPE_PROD_KEY = "pk_live_PuYngGgOA2VX11E7NqNZ1vW3" // Live
